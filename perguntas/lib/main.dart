574261a528e4572d1ca4cc1110b0be4b9d966468
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-26 12:20:09 -0300

    Commitando alterações

--- perguntas/lib/main.dart
+++ perguntas/lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main(){
-    runApp(PerguntaApp());
+  runApp(PerguntaApp());
 }
 
 class PerguntaApp extends StatelessWidget{
-
-    Widget build(BuildContext context){
-        return MaterialApp(
-            home: Scaffold(
-                appBar: AppBar(
-                    title: Text('Perguntas'),
-                ),
-                body: Text('Olá, flutter!'),
+  Widget build(BuildContext context){
+    final List<String> perguntas = [
+      "Qual é a sua cor favorita?",
+      "Qual é o seu animal favorito?",
+      "Qual seu sabor favorito de sorvete?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text("Resposta 1"),
+              onPressed: null,
             ),
-        );
-    }
+          ],
+        ),
+      ),
+    );
+  }
 }
